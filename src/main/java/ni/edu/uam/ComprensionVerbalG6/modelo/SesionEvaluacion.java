99fe8284679b15ad03b1ff187c1e26798817dec9
alee1616 <maguillenc@uamv.edu.ni>
2026-06-13 20:50:07 -0600

    fix: corregir error tabular en la colección de respuestas de SesionEvaluacion

--- src/main/java/ni/edu/uam/ComprensionVerbalG6/modelo/SesionEvaluacion.java
+++ src/main/java/ni/edu/uam/ComprensionVerbalG6/modelo/SesionEvaluacion.java
@@ -37,7 +37,7 @@ public class SesionEvaluacion {
     private Integer puntajeTotal;
 
     @OneToMany(mappedBy = "sesion", cascade = CascadeType.ALL)
-    @ListProperties("pregunta.numero, pregunta.textoPrincipal, opcionElegida.letra")
+    @ListProperties("pregunta, opcionElegida")
     private List<RespuestaSesion> respuestas = new ArrayList<>();
 
 }
