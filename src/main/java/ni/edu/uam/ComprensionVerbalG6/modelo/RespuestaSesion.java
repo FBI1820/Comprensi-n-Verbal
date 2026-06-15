495e6029b8fb1fefc4d71f65c18634fcb2ce24b1
gabi122007 <gabiespr07@gmail.com>
2026-06-15 13:57:15 -0600

    feat: agregar atributo 'MomentoRespuesta' y método 'contestar' en clase RespuestaSesion

--- src/main/java/ni/edu/uam/ComprensionVerbalG6/modelo/RespuestaSesion.java
+++ src/main/java/ni/edu/uam/ComprensionVerbalG6/modelo/RespuestaSesion.java
@@ -13,6 +13,10 @@ import javax.persistence.ManyToOne;
 
 import org.openxava.annotations.*;
 import javax.persistence.*;
+import javax.validation.ValidationException;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
 @Getter
 @Setter
 @Entity
@@ -21,6 +25,7 @@ public class RespuestaSesion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Hidden
     private Long id;
 
     @ManyToOne
@@ -29,14 +34,40 @@ public class RespuestaSesion {
     private SesionEvaluacion sesion;
 
     @ManyToOne
-    @JoinColumn(name = "pregunta_id", nullable = false)
-    @NoCreate @NoModify
-    @DescriptionsList(descriptionProperties = "numero, textoPrincipal")
+    @JoinColumn(name="pregunta_id", nullable=false)
     private Pregunta pregunta;
 
     @ManyToOne
-    @JoinColumn(name = "opcion_id")
-    @DescriptionsList(descriptionProperties = "letra, texto")
+    @JoinColumn(name="opcion_id")
+    @DescriptionsList(descriptionProperties="letra, texto") // solo muestra letra y texto
+    @NoCreate @NoModify @NoSearch
     private Opcion opcionElegida;
+
+    @Hidden
+    private LocalDateTime momentoRespuesta;
+
+    public void setOpcionElegida(Opcion opcion) {
+        if (opcion != null) {
+            contestar(opcion);
+        } else {
+            this.opcionElegida = null;
+        }
+    }
+
+
+    public void contestar(Opcion opcion) {
+        if (!sesion.dentroDelTiempo()) {
+            throw new ValidationException("El tiempo de la sesión ya terminó");
+        }
+        this.opcionElegida = opcion;
+        this.momentoRespuesta = LocalDateTime.now();
+    }
+
+
+
+
+
+
+
 
 }
