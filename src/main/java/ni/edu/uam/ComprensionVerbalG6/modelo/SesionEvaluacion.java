44a2bb2d1946a6b1276c817e6bf47829a4fabdd8
gabi122007 <gabiespr07@gmail.com>
2026-06-15 13:58:23 -0600

    feat: agregar método dentroDelTiempo y finalizarSesion en clase SesionEvaluacion

--- src/main/java/ni/edu/uam/ComprensionVerbalG6/modelo/SesionEvaluacion.java
+++ src/main/java/ni/edu/uam/ComprensionVerbalG6/modelo/SesionEvaluacion.java
@@ -2,6 +2,7 @@ package ni.edu.uam.ComprensionVerbalG6.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
+import ni.edu.uam.ComprensionVerbalG6.calculadores.PuntajeCalculator;
 import org.openxava.annotations.ListProperties;
 
 import javax.persistence.*;
@@ -19,6 +20,7 @@ public class SesionEvaluacion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Hidden
     private Long id;
 
     @Column(name = "nombre_evaluado", nullable = false, length = 200)
@@ -36,18 +38,39 @@ public class SesionEvaluacion {
     @ReadOnly
     private Integer puntajeTotal;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "test_id", nullable = false)
+    @DescriptionsList(descriptionProperties = "tipoTest")
+    private Test test;
+
     @OneToMany(mappedBy = "sesion", cascade = CascadeType.ALL)
-    @ListProperties("pregunta, opcionElegida")
+    @ListProperties("pregunta.pregunta, opcionElegida.texto")
     private List<RespuestaSesion> respuestas = new ArrayList<>();
 
     @PrePersist
     private void registrarFechaInicio() {
         this.fechaInicio = LocalDateTime.now();
+        this.fechaFin = fechaInicio.plusMinutes(test.getTiempoLimiteMinutos());
+
+        this.respuestas = new ArrayList<>();
+        for (Pregunta p : test.getPreguntas()) {
+            RespuestaSesion r = new RespuestaSesion();
+            r.setSesion(this);
+            r.setPregunta(p);
+            respuestas.add(r);
+        }
     }
 
-    public void agregarRespuesta(RespuestaSesion respuesta) {
-        respuestas.add(respuesta);
-        respuesta.setSesion(this);
+
+    public boolean dentroDelTiempo() {
+        return LocalDateTime.now().isBefore(fechaFin);
     }
+
+    public void finalizarSesion() {
+        this.fechaFin = LocalDateTime.now();
+        this.puntajeTotal = PuntajeCalculator.calcular(this);
+    }
+
+
 
 }
