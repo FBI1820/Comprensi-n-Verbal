c93e6cbf949f1a944eca7c8ab0f11fc0e4c06a3a
alee1616 <maguillenc@uamv.edu.ni>
2026-06-22 15:54:08 -0600

    fix: optimizar accion exportar pdf con parametros dinamicos

--- src/main/java/ni/edu/uam/ComprensionVerbalG6/acciones/ExportarResultadoPDF.java
+++ src/main/java/ni/edu/uam/ComprensionVerbalG6/acciones/ExportarResultadoPDF.java
@@ -1,17 +1,22 @@
 package ni.edu.uam.ComprensionVerbalG6.acciones;
 
+import org.apache.commons.logging.*;
 import org.openxava.actions.*;
 
 public class ExportarResultadoPDF extends BaseAction implements IForwardAction {
 
+    private static final Log log = LogFactory.getLog(ExportarResultadoPDF.class);
+
     @Override
     public void execute() throws Exception {
-        System.out.println("====== Redirigiendo al servlet del reporte PDF ======");
+        log.info("ejecutando redirección automatizada hacia el servlet de reportes pdf");
     }
 
     @Override
     public String getForwardURI() {
-        return "/servlets/descargarPdf";
+        // se recupera el nombre del módulo activo en openxava para enviarlo como parámetro al servlet
+        String moduloActivo = getManager().getModuleName();
+        return "/servlets/descargarPdf?module=" + moduloActivo;
     }
 
     @Override
